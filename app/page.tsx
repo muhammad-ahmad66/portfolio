10e1ae01ead6438818b5e0e818c7208f42bdb075
muhammad-ahmad66 <muhammadugv66@gmail.com>
2025-11-17 15:46:06 +0500

    Fix ProcessSection import

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { HeroSection } from "@/components/sections/home/hero-section";
 import { AboutSection } from "@/components/sections/home/about-section";
-import { ProcessSection } from "@/components/sections/home/process-section";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutSection />
-      {/* <ProcessSection /> */}
     </>
   );
 }
